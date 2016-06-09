b479f2a4b7b1bdc8cf22749716aba0ca45bc3ed8
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2016-06-09 12:01:45 +0200

    Add project files.

---
+++ SST C sharp test project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SST_C_sharp_test_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
