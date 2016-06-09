3416819656c7f9fffe2d352cc4b2b108e773ddd5
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2016-06-09 13:24:24 +0200

    Initial commit

--- SST C sharp test project/Program.cs
+++ SST C sharp test project/Program.cs
@@ -10,6 +10,33 @@ namespace SST_C_sharp_test_project
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Simple Sleep Timer C# edition");
+            Console.Write("Input a decimal number: ");
+            string user_input = Console.ReadLine();
+
+            double sleep_time;
+
+            if (double.TryParse(user_input, out  sleep_time) == true){
+                sleep_time = Convert.ToInt32(sleep_time * 3600);
+                string sleep_time_string = Convert.ToString(sleep_time);
+                string cmd_command = "shutdown -s -f -t " + sleep_time_string;
+                Console.WriteLine("Sleep time: " + sleep_time_string);
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c " + cmd_command;
+                process.StartInfo = startInfo;
+                process.Start();
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input");
+            }
+
+
+            System.Threading.Thread.Sleep(3000);
         }
     }
 }
