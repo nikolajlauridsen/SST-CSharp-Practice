4baebf0213e20d2d478c9b64c0888c7eb996888f
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2016-06-09 13:52:58 +0200

    Refactoring existing code

---
+++ SST C sharp test project/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SST_C_sharp_test_project.functions
+{
+    class functions
+    {
+        public static void send_command(string cmd_command)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/c " + cmd_command;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
--- SST C sharp test project/Program.cs
+++ SST C sharp test project/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SST_C_sharp_test_project.functions;
 
 namespace SST_C_sharp_test_project
 {
@@ -16,19 +17,14 @@ namespace SST_C_sharp_test_project
 
             double sleep_time;
 
-            if (double.TryParse(user_input, out  sleep_time) == true){
+            if (double.TryParse(user_input, out  sleep_time) == true)
+            {
                 sleep_time = Convert.ToInt32(sleep_time * 3600);
                 string sleep_time_string = Convert.ToString(sleep_time);
                 string cmd_command = "shutdown -s -f -t " + sleep_time_string;
                 Console.WriteLine("Sleep time: " + sleep_time_string);
 
-                System.Diagnostics.Process process = new System.Diagnostics.Process();
-                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-                startInfo.FileName = "cmd.exe";
-                startInfo.Arguments = "/c " + cmd_command;
-                process.StartInfo = startInfo;
-                process.Start();
+                functions.functions.send_command(cmd_command);
             }
             else
             {
